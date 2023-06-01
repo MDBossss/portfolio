ab36e6d60adfb93c19d5b504f0722fed72347405
MDBossss <ferrari020203@gmail.com>
2023-06-02 00:23:10 +0200

    updated projects

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -1,48 +1,54 @@
-import {AiFillGithub} from "react-icons/ai";
-import {FiExternalLink} from "react-icons/fi";
+import { AiFillGithub } from "react-icons/ai";
+import { FiExternalLink } from "react-icons/fi";
 
-interface Props{
-    project: {
-        id: number,
-        title: string,
-        description: string,
-        githubLink: string,
-        demoLink: string,
-        techStack: string[],
-        image:string
-    }
+interface Props {
+	project: {
+		id: number;
+		title: string;
+		description: string;
+		githubLink: string;
+		demoLink: string;
+		techStack: string[];
+		image: string;
+	};
 }
 
+const Project = ({ project }: Props) => {
+	return (
+		<div className="project">
+			<div className="project-number">
+				<h3>{`PROJECT_0${project.id}`}</h3>
+			</div>
+			<div className="top">
+				<div className="left">
+					<img src={project.image} alt="showcase" />
+				</div>
+				<div className="right">
+					<div className="title">
+						<h2>{project.title}</h2>
+					</div>
+					<div className="description">
+						<p>{project.description}</p>
+					</div>
+					<div className="links">
+						<a className="link" href={project.githubLink} target="_blank">
+							<p>Code</p>
+							<AiFillGithub className="icon" />
+						</a>
+						<a className="link" href={project.demoLink} target="_blank">
+							<p>Live Demo</p>
+							<FiExternalLink className="icon" />
+						</a>
+					</div>
+				</div>
+			</div>
+			{/* <div className="bottom">
+				{project.techStack.map((tech) => (
+					<p className="tech">{tech}</p>
+				))}
+			</div> */}
+		</div>
+	);
+};
 
-const Project = ({project}:Props) => {
-  return (
-    <div className="project">
-        <div className="top">
-            <div className="left">
-                <img src={project.image} alt="showcase" />
-            </div>
-            <div className="right">
-                <div className="title">{project.title}</div>
-                <div className="description">{project.description}</div>
-                <div className="links">
-                    <a className="github" href={project.githubLink} target="_blank">
-                        <p>Code</p>
-                        <AiFillGithub/>
-                    </a>
-                    <a className="demo" href={project.demoLink} target="_blank">
-                        <p>Live Demo</p>
-                        <FiExternalLink/>
-                    </a>
-                </div>
-            </div>
-        </div>
-        <div className="bottom">
-            {project.techStack.map((tech) => (
-                <p className="tech">{tech}</p>
-            ))}
-        </div>
-    </div>
-  )
-}
-
-export default Project
+export default Project;
--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -5,12 +5,40 @@ const projects = [
     {
         id: 1,
         title: "HotelHub",
-        description: "HotelHub is a fullstack hotel room reservation app which allows users to browse, filter and book a variety of hotel rooms across multiple locations.",
+        description: "HotelHub is a fullstack hotel room reservation app which allows users to browse, filter and book a variety of hotel rooms across multiple locations, as well as look at other people's reviews and posts. Built using React + Vite, TypeScript, SASS, Node.js, Express.js and Supabase.",
         githubLink: "https://github.com/MDBossss/HotelHub",
         demoLink: "https://hotel-hub-client.vercel.app/",
         techStack: ["React + Vite","SASS", "TypeScript", "Node.js", "Express.js","Supabase"],
-        image: "/hotelhub-showcase.png"
-    }
+        image: "/hotelhub.png"
+    },
+    {
+        id: 2,
+        title: "Hotcoin",
+        description: "Hotcoin is a React project focused on cryptocurrency data. Using multiple api's it displays latest crypto-related news as well as top 100 cryptocurrencies along with their price data. The app utilizes Firebase for user auth and data storage.",
+        githubLink: "https://github.com/MDBossss/hotcoin",
+        demoLink: "https://hotcoin.vercel.app/",
+        techStack: ["React","Firebase"],
+        image: "/hotcoin.png"
+    },
+    {
+        id: 3,
+        title: "xoOnline",
+        description: "xoOnline is a simple tic-tac-toe game with online multiplayer support where players can create new rooms and seamlessly connect to existing ones with a quick-join feature. Built using React + Vite, SASS, Node.js, Express.js and socket.io",
+        githubLink: "https://github.com/MDBossss/xoOnline",
+        demoLink: "https://hotel-hub-client.vercel.app/",
+        techStack: ["React + Vite","SASS", "Node.js", "Express.js","socket.io"],
+        image: "/xoonline.png"
+    },
+    
+    {
+        id: 4,
+        title: "Span Hackathon 2023",
+        description: "This is an unfinished full stack cyber security app made by a team of 5 during Span's 24h Hackathon, which got awarded 3rd place. Built with React + Vite, SASS, Firebase, Node.js, Express.js, MySQL. ",
+        githubLink: "https://github.com/Lovakovic/spanHackathon",
+        demoLink: "https://hotcoin.vercel.app/",
+        techStack: ["React + Vite","SASS", "Firebase", "Node.js","Express.js","MySQL"],
+        image: "/hackathon.png"
+    },
 ]
 
 
@@ -18,7 +46,7 @@ const Projects = () => {
   return (
     <div className="container projects">
         {projects.map((project) => (
-            <Project project={project}/>
+            <Project key={project.id} project={project}/>
         ))}
     </div>
   )
